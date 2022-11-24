24c0be240b8d465ca05f0ae2388aa3064433d8f4
Haïthem BEN AYOUB <instinctbuilding@gmail.com>
2022-11-24 19:00:49 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fa978bb1-7bbe-441d-9ab1-9a170c7529b9';
+export const MODAL_CLOSE_SEC = 2.5;
+
+/**
+ * @API_URL : It is the url link to API located in the serber
+ * @TIMEOUT_SEC :
+ */
